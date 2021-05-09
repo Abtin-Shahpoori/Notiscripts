e0caf90fd429dc8d26c5a4e5d7fab96df5d12921
AbtinShahpoori <abtin.shahpoori.monfared@gmail.com>
2021-05-09 17:35:56 +0430

    Added color and text-decoration

---
+++ GetStarted.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+#defining colors
+Black="\u001b[30m"; Red="\u001b[31m"
+Green="\u001b[32m"; Yellow="\u001b[33m"
+Blue="\u001b[34m";  Magneta="\u001b[35m"
+Cyan="\u001b[36m";  white="\u001b[37m"
+
+#defining Decorations
+Bold="\u001b[1m"; UnderLine="\u001b[4m"
+Italics="u001b[3m"; Reset="\u001b[0m"
