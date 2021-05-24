aaa6dd4de534a25d06d12671415ba7fbb1b9f229
AbtinShahpoori <abtin.shahpoori.monfared@gmail.com>
2021-05-24 18:45:31 +0430

    Added Idle alerts

--- HealthScript.py
+++ HealthScript.py
@@ -2,8 +2,7 @@ import gi
 import yaml
 import os
 import time
-from BirthdayScript import *
-from datetime import datetime
+from datetime import date, datetime
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify, GdkPixbuf
 Notify.init("Random");
@@ -30,6 +29,8 @@ idleAlertStatus = User.get("idleAlert");
 # Notification Icons initrializing
 lunchIcon = GdkPixbuf.Pixbuf.new_from_file("./clipArts/Apple.png")
 bedIcon = GdkPixbuf.Pixbuf.new_from_file("./clipArts/Bed.png")
+IdleAlert = GdkPixbuf.Pixbuf.new_from_file("./clipArts/chair.png")
+
 # Notifications texts
 lunchNotif = Notify.Notification.new(
     "Lunch time",
@@ -46,16 +47,23 @@ bedNotif = Notify.Notification.new(
     "sometime the most productive thing one can do is to sleep",
 )
 
+iaNotif = Notify.Notification.new(
+    "Get up",
+    "Sitting for too long increases your risk of chronic health problems, such as heart disease, diabetes and some cancers"
+)
+
 # Assigning Icons to notifications
-lunchNotif.set_icon_from_pixbuf(lunchIcon);
-dinnerNotif.set_icon_from_pixbuf(lunchIcon);
-bedNotif.set_icon_from_pixbuf(bedIcon);
+lunchNotif.set_icon_from_pixbuf(lunchIcon)
+dinnerNotif.set_icon_from_pixbuf(lunchIcon)
+bedNotif.set_icon_from_pixbuf(bedIcon)
+iaNotif.set_icon_from_pixbuf(IdleAlert)
 
 age = 0
-x = False
+Birthday = True
+iaTime = datetime.now().strftime("%H%M")
+iaTime = int(iaTime);
 while True:
     time.sleep(0.2)
-    # making age global
     fullDate = datetime.now()
 
     # Fixed elements
@@ -67,10 +75,11 @@ while True:
     # Fix hour dates
     cFull   = datetime.now()
     cDate   = [cFull.strftime("%Y"), cFull.strftime("%m"), cFull.strftime("%d")]
-    cTime   = [cFull.strftime("%H"), cFull.strftime("%M")] 
-    
+    cTime   = [cFull.strftime("%H"), cFull.strftime("%M")]
+    ciaTime = cFull.strftime("%H%M")
     # Fix age variable
-    age = int(cDate[0]) - int(fixedDOB[0]);
+    age = (int(cDate[0]) - int(fixedDOB[0]));
+    
     try:
         fAge = open(".Age.txt", "w")
         fAge.write(f"{age}")
@@ -94,10 +103,10 @@ while True:
         lunchNotif.show()
         time.sleep(60)
 
-    if fixedDOB == cDate and x == True:
+    if fixedDOB == cDate and Birthday == True:
         # Run HappyBirthday Script
         os.system("python3 ./BirthdayScript")
-        x = False
+        Birthday = False
 
     if fixedDT == cTime:
         # Run Dinnertime script
@@ -109,4 +118,14 @@ while True:
         time.sleep(60)
         # Run bedTime Script
     
-    # TODO: create the idleAlert 
+    #Defining idle alert time
+    ciaTime = int(ciaTime)
+    iaC = ciaTime - iaTime
+    print(iaC)
+    if iaC % 100 == 0 and iaC > 0:
+        #run idle alert
+        iaNotif.show()
+        time.sleep(60)
+
+    # TODO: create the idleAlert
+    
